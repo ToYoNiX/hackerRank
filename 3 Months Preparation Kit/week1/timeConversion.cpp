e259853d03e385066c903c5f8feaa473f501f26f
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-09-09 13:06:22 +0300

    </>

---
+++ 3 Months Preparation Kit/week1/timeConversion.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    if (s[0] == '1' && s[1] == '2') {
+        if (s[8] == 'A'){
+            s[0] = '0';
+            s[1] = '0';   
+        }
+    } else if (s[8] == 'P') {
+        s[0] += 1;
+        s[1] += 2;
+    }
+    s.erase(8,9);
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
